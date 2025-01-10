b0fac1e3da8fb364229296a189cd8acc0d289121
ROASTEROP <kasimfriend156@gmail.com>
2025-01-10 13:44:25 +0530

    DK

--- utils/vars.py
+++ utils/vars.py
@@ -1,5 +1,7 @@
 MD_FORCE_SUB = ""
 FORCESUB_TXT = """
+Kya re bhikmangya?! Aa gya Dekhne apna nanga naach? Maa Baap ki gaaliya kam thi ki yha ab ek nangi erotic bot aurat se apni g*** marwaega, Kuch toh sharam kr bhadwe! 
+Sorry for the Deepak Kalal Mischievance. I'm your Bot guide "Yor" Let's start this erotic journey of yours with me.
 
 """
 
